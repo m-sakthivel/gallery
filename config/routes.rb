0c8a00dd8abb9f51f83a1575dbeb11eb34800ba0
’sakthivel87’ <muthukrishnansakthivel@gmail.com>
2015-02-11 18:17:54 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'upload_pics#index'
+   root :to => 'upload_pics#dashboard'
 
   # See how all your routes lay out with "rake routes"
 
